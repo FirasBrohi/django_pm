1cb1193001da3878473a6186d129fa52fa387dbe
firas abdulhadi <filas2019@gmail.co>
2024-10-04 21:28:48 +0300

    search and pagination

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from django.views.generic import ListView,CreateView,UpdateView,DeleteView
 from . import models
 from . import forms
@@ -8,6 +9,16 @@ class ProjectListView(ListView):
     model  = models.Project
 
     template_name ='project/list.html'
+
+    paginate_by = 3
+
+    def get_queryset(self):
+        query_set = super().get_queryset()
+        where =  {}
+        q = self.request.GET.get('q', None)
+        if q: 
+            where['title__icontains'] = q
+        return query_set.filter(**where)
     
 class ProjectCreateView(CreateView):
     model = models.Project
